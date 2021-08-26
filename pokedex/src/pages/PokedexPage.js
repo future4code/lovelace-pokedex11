98fb1435071d1ae8b1fec9edb64290bf64e57930
matheus1810 <matheussouza1810@gmail.com>
2021-08-26 15:53:32 -0300

    botao arrumado

--- pokedex/src/pages/PokedexPage.js
+++ pokedex/src/pages/PokedexPage.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { useHistory } from "react-router"
 import Header from "../components/Header"
-
+import { goHome } from "../routes/cordinator"
 
 export const PokedexPage =()=>{
+    const history = useHistory()
     return(<>
         <Header/>
        <h1>Pokedex Page</h1>
+       <button onClick={()=>{goHome(history)}}> comprar mais pokemons</button>
    </>
    
     )
--- pokedex/src/routes/cordinator.js
+++ pokedex/src/routes/cordinator.js
@@ -1,3 +1,9 @@
 export const goPokedex = (history) => {
     history.push('/pokedex');
   };
+
+  export const goHome = (history) => {
+    history.push('/');
+  };
+
+  
